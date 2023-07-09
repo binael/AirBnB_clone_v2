bb61a241d4459c67408d282b7839a2072ecc736f
binael <nbinael@yahoo.com>
2023-07-09 15:19:45 +0100

    adjusted file permissions

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -22,4 +22,5 @@ def do_pack():
     if result.failed:
         return None
     else:
+        local('chmod 664 versions/{}'.format(my_file))
         return 'versions/{}'.format(my_file)
--- webserver.sh
+++ webserver.sh
@@ -1,27 +1,17 @@
 #!/usr/bin/env bash
 # A script that configures nginx and sets up web static as per specifications
-sudo apt-get -y update
 
-if ! sudo command -v nginx &> /dev/null; then
-	sudo apt-get -y install nginx
-fi
-
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-
-echo "Testing nginx installation" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-
-if [ -L /data/web_static/current/ ]; then
-	sudo rm -R /data/web_static/current/
-fi
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
-
-sudo chown -R ubuntu:ubuntu /data/
-
-loc="location /hbnb_static {"
-al="alias /data/web_static/current/;"
-
-sudo sed -i "s/server_name localhost;/server_name _;/" /etc/nginx/sites-available/default
-sudo sed -i "s@root /var/www/html;@root /data/web_static/current;@" /etc/nginx/sites-available/default
-sudo sed -i "/server_name _;/a \\\n\t$loc\n\t\t$al\n\t}" /etc/nginx/sites-available/default
-sudo systemctl restart nginx
+# sudo apt-get -y install pip
+# sudo pip3 uninstall Fabric
+# sudo apt-get -y install libffi-dev
+# sudo apt-get -y install libssl-dev
+# sudo apt-get -y install build-essential
+# sudo apt-get -y install python3.4-dev
+# sudo apt-get -y install libpython3-dev
+sudo pip3 install pyparsing
+sudo pip3 install appdirs
+sudo pip3 install setuptools==40.1.0
+sudo pip3 install cryptography==2.8
+sudo pip3 install bcrypt==3.1.7
+sudo pip3 install PyNaCl==1.3.0
+sudo pip3 install Fabric3==1.14.post1

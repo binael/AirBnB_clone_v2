aa26fe07a1074dc8f786292cd4f2ee4a0e64f97b
binael <nbinael@yahoo.com>
2023-07-24 03:43:15 +0100

    webflask hello

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""A python script that uses web flask to host web application
+"""
+
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """A function that displays greetings"""
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    os.environ['FLASK_ENV'] = 'development'
+    app.run(host='0.0.0.0', port=5000)
